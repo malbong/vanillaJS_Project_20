de7249e9f16cd81621c55a8d05ec0509f6832457
Heegon Ryu <heryu@c6r6s1.42seoul.kr>
2023-08-21 13:48:35 +0900

    made 02 project

---
+++ 02_MovieSeatBooking/main.js
@@ -0,0 +1,75 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const movieSeats = document.querySelector(".movie__seats");
+  const totalCount = document.querySelector(".movie__total #count");
+  const totalPrice = document.querySelector(".movie__total #price");
+  const selectedMovie = document.querySelector("#movies");
+  const seats = document.querySelectorAll(".movie__seats .row .seat");
+  const clearBtn = document.querySelector(".movie__clearBtn");
+
+  const updateTotal = function () {
+    let selectedCount = 0;
+    const selectedIndices = [];
+    seats.forEach((seat, index) => {
+      if (seat.classList.contains("selected")) {
+        selectedCount++;
+        selectedIndices.push(index);
+      }
+    });
+
+    totalCount.textContent = selectedCount;
+    totalPrice.textContent = selectedCount * selectedMovie.value;
+
+    localStorage.setItem("selectedMovie", selectedMovie.selectedIndex);
+    localStorage.setItem(
+      `selectedSeats${selectedMovie.selectedIndex}`,
+      JSON.stringify(selectedIndices)
+    );
+  };
+
+  const seatsInit = function () {
+    const selectedMovieIndex = localStorage.getItem("selectedMovie");
+    if (selectedMovieIndex !== null) {
+      selectedMovie.selectedIndex = selectedMovieIndex;
+    }
+
+    const selectedSeats = JSON.parse(
+      localStorage.getItem(`selectedSeats${selectedMovieIndex}`)
+    );
+    if (selectedSeats !== null) {
+      seats.forEach((seat, index) => {
+        if (selectedSeats.indexOf(index) !== -1) {
+          seat.classList.add("selected");
+        } else {
+          seat.classList.remove("selected");
+        }
+      });
+    }
+    updateTotal();
+  };
+
+  selectedMovie.addEventListener("change", () => {
+    localStorage.setItem("selectedMovie", selectedMovie.selectedIndex);
+    seatsInit();
+  });
+
+  movieSeats.addEventListener("click", (event) => {
+    if (
+      event.target.classList.contains("seat") &&
+      !event.target.classList.contains("occupied")
+    ) {
+      event.target.classList.toggle("selected");
+      updateTotal();
+    }
+  });
+
+  clearBtn.addEventListener("click", () => {
+    seats.forEach((seat) => {
+      if (seat.classList.contains("selected")) {
+        seat.classList.remove("selected");
+      }
+    });
+    updateTotal();
+  });
+
+  seatsInit();
+});
