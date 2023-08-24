ab28264bf8c7b9f306dcc8b981ae03c3abb364d9
Heegon Ryu <heryu@c6r8s1.42seoul.kr>
2023-08-24 11:46:06 +0900

    made 06 project

--- 06_MyLandingPage/main.js
+++ 06_MyLandingPage/main.js
@@ -3,8 +3,57 @@ document.addEventListener("DOMContentLoaded", () => {
   const container = document.querySelector(".container");
   const toggleBtn = document.querySelector("button.toggleBtn");
 
-  toggleBtn.addEventListener("click", () => {
-    nav.classList.toggle("nav__active");
-    container.classList.toggle("nav__active");
+  const signupBtn = document.querySelector(".signupBtn");
+  const modal = document.querySelector(".modal__container");
+  const modalCloseBtn = document.querySelector(".modal__closeBtn");
+
+  const navCloseEvent = function (e) {
+    if (!nav.classList.contains("nav__active")) return;
+
+    let element = e.target;
+    while (element !== document.body) {
+      if (element === nav) {
+        return;
+      }
+      element = element.parentElement;
+    }
+    closeNavbar();
+  };
+
+  const openNavbar = function () {
+    nav.classList.add("nav__active");
+    container.classList.add("nav__active");
+
+    document.body.addEventListener("click", navCloseEvent);
+  };
+
+  const closeNavbar = function (e) {
+    nav.classList.remove("nav__active");
+    container.classList.remove("nav__active");
+
+    document.body.removeEventListener("click", navCloseEvent);
+  };
+
+  toggleBtn.addEventListener("click", (event) => {
+    event.stopPropagation();
+
+    if (nav.classList.contains("nav__active")) closeNavbar();
+    else openNavbar();
   });
+
+  const openModal = function () {
+    modal.classList.add("modal__active");
+    document.body.classList.add("modal__active");
+  };
+
+  const closeModal = function () {
+    modal.classList.remove("modal__active");
+    document.body.classList.remove("modal__active");
+  };
+
+  signupBtn.addEventListener("click", openModal);
+  modal.addEventListener("click", (e) => {
+    if (e.target === modal) closeModal();
+  });
+  modalCloseBtn.addEventListener("click", closeModal);
 });
